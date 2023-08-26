8e9df3de6c2ef7ed3f963fcbf299ba6e9ea1d6ad
Achal Singhal <achals2002@gmail.com>
2023-08-26 17:39:52 +0530

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+	import '../app.css';
+	import { themeChange } from 'theme-change';
+	import { onMount } from 'svelte';
+
+	// NOTE: the element that is using one of the theme attributes must be in the DOM on mount
+	onMount(() => {
+		themeChange(false);
+		// 👆 false parameter is required for svelte
+	});
+</script>
+
+<slot />
+<!-- <input
+	type="checkbox"
+	class="toggle"
+	data-toggle-theme="retro,coffee"
+	data-act-class="ACTIVECLASS"
+/> -->
