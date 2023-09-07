617cddf981fd12e30ad0240cf031a9fba8da32c9
Achal Singhal <achals2002@gmail.com>
2023-09-07 18:19:11 +0530

    some more handling

--- src/lib/server/serverUtils.ts
+++ src/lib/server/serverUtils.ts
@@ -1,13 +1,18 @@
-import type { Cookies } from "@sveltejs/kit";
-import { prisma } from "./prisma";
+import type { Cookies } from '@sveltejs/kit';
+import { prisma } from './prisma';
 
 export const PostAuth = async (request: Request) => {
 	const sessionToken = request.headers.get('Authorization')?.split(' ')[1];
-	const session = await prisma.session.findUnique({
-		where: {
-			sessionToken: sessionToken
-		}
-	});
+	const session = await prisma.session
+		.findUnique({
+			where: {
+				sessionToken: sessionToken
+			}
+		})
+		.catch((err) => {
+			console.log(`Error getting session: ${err}`);
+			return false;
+		});
 	if (!session || session.expires < new Date()) {
 		return false;
 	}
@@ -16,11 +21,16 @@ export const PostAuth = async (request: Request) => {
 
 export const GetAuth = async (cookies: Cookies) => {
 	const sessionToken = cookies.get('next-auth.session-token');
-	const session = await prisma.session.findUnique({
-		where: {
-			sessionToken: sessionToken
-		}
-	});
+	const session = await prisma.session
+		.findUnique({
+			where: {
+				sessionToken: sessionToken
+			}
+		})
+		.catch((err) => {
+			console.log(`Error getting session: ${err}`);
+			return false;
+		});
 	if (!session || session.expires < new Date()) {
 		return false;
 	}
